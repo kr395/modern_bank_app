7723cb5787139eeae9610582bf386bc10f12cc9c
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-11-30 12:43:39 +0530

    completed till hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./style";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 const App = () => (
-    <div className='bg-primary w-full overflow-hidden'>
-    <div className="div">
-      
+  <div className="bg-primary w-full overflow-hidden">
+    {/* Navbar Section */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        < Navbar />
+      </div>
     </div>
-    <h1>hello</h1>
-      
-    </div>
-  )
+    
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        < Hero />
+      </div>
 
-export default App
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        < Stats />
+        < Business />
+        < Billing />
+        < CardDeal />
+        < Testimonials />
+        < Clients />
+        < CTA />
+        < Footer />
+      </div>
+
+    </div>
+
+
+
+
+  </div>
+);
+
+export default App;
