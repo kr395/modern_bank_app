8501c258ddc364c2ebf6adbc183c621248843e79
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-01-18 01:14:48 +0530

    Working on Business Section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,14 +1,24 @@
-import { features } from "../constant"
-import styles, {layout} from "../style"
-import Button from "./Button"
+import { features } from "../constant";
+import styles, { layout } from "../style";
+import Button from "./Button";
 const Business = () => {
   return (
-    <section id="features" className={layout.section} >
-    <div className={`${layout.sectionInfo}`}>
-       <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /> we’ll handle the money.</h2>
-    </div>
-    </section>
-  )
-}
+    <section id="features" className={layout.section}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we’ll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
 
-export default Business
+    </section>
+  );
+};
+
+export default Business;
